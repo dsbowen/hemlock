e3dae213d42760357df8d24f1bbbd51700611ec3
dsbowen <dsbowen@wharton.upenn.edu>
2019-01-18 13:20:21 -0500

    databse

--- hemlock/__init__.py
+++ hemlock/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from hemlock.config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
 
 from hemlock import routes
--- hemlock/config.py
+++ hemlock/config.py
@@ -1,6 +1,10 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = 'hello-world'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        # 'postgres:///'+os.path.join(basedir, 'data.db')
+    # SQLALCHEMY_DATABASE_URI = 'postgres:///'+os.path.join(basedir, 'data.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
