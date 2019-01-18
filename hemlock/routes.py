cb9ecf3c3300a6e1b62d27c3f149df587f69b04a
dsbowen <dsbowen@wharton.upenn.edu>
2019-01-18 15:22:44 -0500

    csv first pass

--- hemlock/routes.py
+++ hemlock/routes.py
@@ -1,4 +1,8 @@
+from flask import make_response
 from hemlock import app, db
+from hemlock.models.question import Question
+import io
+import csv
 import survey
 
 @app.route('/')
@@ -7,3 +11,21 @@ def index():
     db.session.add(survey.q)
     db.session.commit()
     return survey.q.text
+    
+@app.route('/download')
+def download():
+    data = Question.query.get(1).text
+    
+    si = io.StringIO()
+    cw = csv.writer(si)
+    cw.writerow([data])
+    output = make_response(si.getvalue())
+    output.headers['Content-Disposition'] = 'attachment; filename=data.csv'
+    output.headers['Context-type'] = 'text/csv'
+    return output
+    # with open('test.csv', 'w') as test_file:
+        # x = csv.writer(test_file, delimiter=' ', quotechar='|', quoting = csv.QUOTE_MINIMAL)
+        # x.writerow(['hello world'])
+        # return Response(test_file, mimetype='text/csv')
+        # return send_file(test_file, mimetype='text/csv', attachment_filename='test.csv', as_attachment=True)
+    # return data
