3d3529f961cf0f99379e3342d5ec656282756771
dsbowen <dsbowen@wharton.upenn.edu>
2019-01-21 13:42:01 -0500

    new survey

--- survey.py
+++ survey.py
@@ -9,31 +9,40 @@ from hemlock.models.page import Page
 from hemlock.models.question import Question
 
 def Start():
-    b = Branch(next=IceCream)
+    b = Branch(next=Thanks)
+    
     p = Page(branch=b)
-    q = Question(page=p, text='Welcome to Hemlock')
-    first_name = Question(page=p, var='first_name', qtype='free', text="What's your first name?", all_rows=True)
-    last_name = Question(page=p, var='last_name', qtype='free', text="What's your last name?", all_rows=True)
-    b.set_args([first_name.id, last_name.id])
+    q = Question(page=p, text='Welcome to Hemlock!')
+    q = Question(page=p, text="Let's start with a some simple free response questions.")
+    
+    p = Page(branch=b)
+    first = Question(page=p, var='first_name', qtype='free', all_rows=True)
+    first.set_text("What's your first name?")
+    last = Question(page=p, var='last_name', qtype='free', all_rows=True)
+    last.set_text("...and your last name?")
+    
+    b.set_args([first.id, last.id])
+    
     return b
     
-def IceCream(args):
-    first_id, last_id = args
-    first_name, last_name = Question.query.filter(Question.id.in_(args))
-    
-    b = Branch(next=End)
-    p = Page(branch=b)
-    q = Question(page=p, text='Welcome, '+first_name.data+' '+last_name.data+'!')
-    q = Question(page=p, text='What are your three favorite flavors of ice cream?')
-    for i in range(3):
-        q = Question(page=p, var='ice_cream', qtype='free')
-    p = Page(branch=b)
-    q = Question(page=p, var='cereal', qtype='free', text='What is your favorite cereal?')
-    return b
-    
-def End():
+def ice_cream(page, index):
+    q = Question(page=page, var='ice_cream', qtype='free')
+    q.set_text('Favorite ice cream number '+str(index))
+
+def Thanks(name_ids):
     b = Branch()
+    
     p = Page(branch=b)
-    p.set_terminal()
-    q = Question(page=p, text='Thank you for taking this survey!!')
+    first, last = Question.query.filter(Question.id.in_(name_ids))
+    q = Question(page=p, text='Thanks for taking this survey, '+first.data+' '+last.data+'!')
+    q = Question(page=p, text="Let's continue to some more simple free response questions.")
+    
+    p = Page(branch=b)
+    q = Question(page=p, text="Alright, now, what are your three favorite flavors of ice cream?")
+    for i in range(3):
+        ice_cream(p, i+1)
+        
+    p = Page(branch=b, terminal=True)
+    q = Question(page=p, text="Thank you for taking my survey!")
+    
     return b
