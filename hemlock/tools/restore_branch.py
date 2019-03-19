6863710daa44f87ea39b58342f233b491631fd6f
dsbowen <dsbowen@wharton.upenn.edu>
2019-03-19 14:15:11 -0400

    deleted restore_branch

--- hemlock/tools/restore_branch.py
+++
@@ -1,8 +0,0 @@
-
-
-from hemlock.tools.query import query
-from hemlock.models.branch import Branch
-
-def restore_branch(prev_id, table=Branch):
-    curr_id = query(prev_id,table).get_next_branch_id()
-    return query(curr_id, Branch)
