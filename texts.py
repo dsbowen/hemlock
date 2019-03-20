940189ab2c37d4676437c0cffbf63bb6d27825a6
dsbowen <dsbowen@wharton.upenn.edu>
2019-03-20 17:35:58 -0400

    master commit

--- texts.py
+++ texts.py
@@ -5,14 +5,6 @@
 ###############################################################################
 
 consent = '''
-<p>We are researchers at the University of Pennsylvania and we are studying decision making.</p>
 
-<p>This survey was created using new software that we designed. Although we have made every effort to ensure the survey progresses smoothly, you may experience problems with the software.</p>
-
-<p>If you encounter any problems during the survey, such as an 'Internal Server Error' or lag times exceeding 5 seconds, we ask that you email us at dsbowen@wharton.upenn.edu. In your email, please briefly describe the problem and specify the last question you remember answering before the problem occurred.</p>
-
-<p>We will make every effort to keep all the information you tell us during the study strictly confidential, except as required by law. Your name and other identifying information will never be connected with the responses you provide so no one will ever be able to identify you in any publications that will result from this research. If you have questions about your rights as a volunteer in this research study you can contact the Office of Regulatory Affairs at the University of Pennsylvania at (215)-898-2614.</p>
-
-<p>By continuing with this survey, you acknowledge that you have read the information above and are consenting to participate.</p>
 '''
 
