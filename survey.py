37046656c6acd51d4db01783de1b8cdcf5432d2b
dsbowen <dsbowen@wharton.upenn.edu>
2019-04-30 17:05:21 -0400

    cleaned survey

--- survey.py
+++ survey.py
@@ -1,9 +1,9 @@
 ###############################################################################
 # Hemlock survey template
 # by Dillon Bowen
-# last modified 04/05/2019
+# last modified 04/29/2019
 ###############################################################################
-'''
+
 # import hemlock package, configuration class, and texts
 from hemlock import *
 from config import Config
@@ -28,36 +28,4 @@ if __name__ == '__main__':
     
 # hemlock shell
 import hemlock_shell
-'''
 
-# import hemlock package, configuration class, and texts
-from hemlock import *
-from config import Config
-from texts import *
-from flask_login import current_user
-
-def Start():
-    b = Branch()
-    p = Page(b)
-    q = Question(p, 'hello world')
-    p = Page(b)
-    q = Question(p, 'hello moon')
-    p = Page(b, terminal=True)
-    q = Question(p, 'goodbye world')
-    return b
-      
-# create the application (survey)
-app = create_app(
-    Config,
-    start=Start,
-    password='',
-    record_incomplete=True,
-    block_duplicate_ips=False,
-    block_from_csv='block.csv')
-    
-# run app
-if __name__ == '__main__':
-    app.run()
-    
-# hemlock shell
-import hemlock_shell
