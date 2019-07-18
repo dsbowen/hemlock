080d453e9181c490ee0056d1db8076c82a9101d5
dsbowen <dsbowen@wharton.upenn.edu>
2019-07-18 12:53:17 -0400

    study

--- survey.py
+++ survey.py
@@ -3,7 +3,7 @@
 # by Dillon Bowen
 # last modified 04/29/2019
 ###############################################################################
-
+'''
 # import hemlock package, configuration class, and texts
 from hemlock import *
 from config import Config
@@ -12,6 +12,35 @@ from flask_login import current_user
 
 def Start():
     raise NotImplementedError()
+      
+# create the application (survey)
+app = create_app(
+    Config,
+    start=Start,
+    password='',
+    record_incomplete=False,
+    block_duplicate_ips=False,
+    block_from_csv='block.csv')
+    
+# run app
+if __name__ == '__main__':
+    app.run()
+    
+# hemlock shell
+import hemlock_shell
+'''
+
+# import hemlock package, configuration class, and texts
+from hemlock import *
+from config import Config
+from texts import *
+from flask_login import current_user
+
+def Start():
+    b = Branch()
+    p = Page(b, terminal=True)
+    q = Question(p, 'Hello World')
+    return b
       
 # create the application (survey)
 app = create_app(
