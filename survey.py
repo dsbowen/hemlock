5ce61b33ac4789ae1df4e5bae916571ddebfe648
dsbowen <dsbowen@wharton.upenn.edu>
2019-07-21 21:31:35 -0400

    restored survey

--- survey.py
+++ survey.py
@@ -3,7 +3,7 @@
 # by Dillon Bowen
 # last modified 04/29/2019
 ##############################################################################
-'''
+
 # import hemlock package, configuration class, and texts
 from hemlock import *
 from config import Config
@@ -13,49 +13,6 @@ from flask_login import current_user
 def Start():
     raise NotImplementedError()
       
-# create the application (survey)
-app = create_app(
-    Config,
-    start=Start,
-    password='',
-    record_incomplete=False,
-    block_duplicate_ips=False,
-    block_from_csv='block.csv')
-    
-# run app
-if __name__ == '__main__':
-    app.run()
-    
-# hemlock shell
-import hemlock_shell
-'''
-
-# import hemlock package, configuration class, and texts
-from hemlock import *
-from config import Config
-from texts import *
-from flask_login import current_user
-
-def Start():
-    b = Branch()
-    p = Page(b, fail)
-    q = Question(p, "What's your first name?", qtype='free', var='first')
-    Validator(q, require)
-    q = Question(p, "What's your last name?", qtype='free', var='last')
-    Validator(q, require)
-    p = Page(b)
-    q = Question(p, "What's your favorite flavor of ice cream?", qtype='single choice', var='ice_cream')
-    Choice(q, 'Lavender')
-    Choice(q, 'Chocolate')
-    Choice(q, 'Orange')
-    Validator(q, require)
-    p = Page(b, terminal=True)
-    q = Question(p, 'Hello World')
-    return b
-
-def fail(p):
-    x = 1/0
-
 # create the application (survey)
 app = create_app(
     Config,
