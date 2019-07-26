1e786085e46be449f561b0880b178ead26808e60
dsbowen <dsbowen@wharton.upenn.edu>
2019-07-26 13:34:12 -0400

    trying something

--- config.py
+++ config.py
@@ -13,6 +13,7 @@ class Config(object):
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
 		'sqlite:///'+os.path.join(basedir, 'data.db')
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
+    WKHTMLTOPDF_CMD = os.environ.get('WKHTMLTOPDF_BINARY')
 	# WKHTMLTOPDF_CMD = subprocess.Popen(
 		# ['which', os.environ.get('WKHTMLTOPDF_BINARY', 'wkhtmltopdf')],
 		# stdout=subprocess.PIPE).communicate()[0].strip()
