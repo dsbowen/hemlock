4cffd244ec782ec42a196fb6b350ab44b8d98e0c
dsbowen <dsbowen@wharton.upenn.edu>
2019-07-26 13:45:01 -0400

    tab to space in config

--- config.py
+++ config.py
@@ -9,11 +9,11 @@ import subprocess
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-	SECRET_KEY = os.environ.get('SECRET_KEY') or 'uA%G?{wVEH&`@As'
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-		'sqlite:///'+os.path.join(basedir, 'data.db')
-	SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'uA%G?{wVEH&`@As'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///'+os.path.join(basedir, 'data.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     WKHTMLTOPDF_CMD = os.environ.get('WKHTMLTOPDF_BINARY')
-	# WKHTMLTOPDF_CMD = subprocess.Popen(
-		# ['which', os.environ.get('WKHTMLTOPDF_BINARY', 'wkhtmltopdf')],
-		# stdout=subprocess.PIPE).communicate()[0].strip()
+    # WKHTMLTOPDF_CMD = subprocess.Popen(
+        # ['which', os.environ.get('WKHTMLTOPDF_BINARY', 'wkhtmltopdf')],
+        # stdout=subprocess.PIPE).communicate()[0].strip()
