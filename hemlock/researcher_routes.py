e82aaccde5b6fbcc3e644027f52665c3e743790c
dsbowen <dsbowen@wharton.upenn.edu>
2019-07-31 02:51:52 -0400

    bootstrap only

--- hemlock/researcher_routes.py
+++ hemlock/researcher_routes.py
@@ -112,11 +112,11 @@ def download_survey(part_id):
         for p in compiled_html]
     basedir = os.getcwd()
     css = [basedir+url_for('static', filename='css/'+css_file)
-        for css_file in ['bootstrap.min.css', 'default.min.css']]
+        for css_file in ['bootstrap.min.css']]
     config = imgkit.config(wkhtmltoimage='/app/bin/wkhtmltoimage')
     # images = [imgkit.from_string(html, False, css=css, config=config) 
         # for html in compiled_html]
-    images = [imgkit.from_string(html, False, config=config) 
+    images = [imgkit.from_string(html, False, css=css, config=config) 
         for html in compiled_html]
     zipf = zipfile.ZipFile('survey.zip', 'w', zipfile.ZIP_DEFLATED)
     [zipf.writestr('page{}.png'.format(i), img)
