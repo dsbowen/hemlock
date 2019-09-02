72b90795e9ecab44effaf466e9f27c9d294e5609
dsbowen <dsbowen@wharton.upenn.edu>
2019-09-02 15:46:05 -0400

    table working in survey view

--- survey.py
+++ survey.py
@@ -14,6 +14,42 @@ from flask_login import current_user
 
 def Start():
     raise NotImplementedError()
+    
+table = '''
+<center>
+<table style="margin-left: auto; margin-right: auto; border-collapse: collapse;" border="1px">
+<tbody>
+<tr style="height: 35px;">
+<td style="text-align: center;" colspan="3">Export to Word</td>
+<tr style="height: 35px;">
+<td style="text-align: center; width: 130px;"></td>
+<td style="text-align: center; width: 130px;">Hemlock</td>
+<td style="text-align: center; width: 130px;">Qualtrics</td>
+</tr>
+<tr style="height: 35px;">
+<td style="text-align: center;">Local images</td>
+<td style="text-align: center;">Yes</td>
+<td style="text-align: center;">Yes</td>
+</tr>
+<tr style="height: 35px;">
+<td style="text-align: center;">Images from URL</td>
+<td style="text-align: center;">Yes</td>
+<td style="text-align: center;">Yes</td>
+</tr>
+<tr style="height: 35px;">
+<td style="text-align: center;">Videos</td>
+<td style="text-align: center;">Yes</td>
+<td style="text-align: center;">No</td>
+</tr>
+<tr style="height: 35px;">
+<td style="text-align: center;">Tables</td>
+<td style="text-align: center;">Yes</td>
+<td style="text-align: center;">No</td>
+</tr>
+</tbody>
+</table>
+</center>
+'''
         
 def Start():
     b = Branch()
@@ -32,6 +68,9 @@ def Start():
     p = Page(b)
     url = 'https://www.youtube.com/watch?v=cpsJw26b3dM'
     q = Question(p, video(url, parms={'autoplay':1}))
+    
+    p = Page(b)
+    q = Question(p, table)
     
     p = Page(b, terminal=True)
     q = Question(p, 'The End')
