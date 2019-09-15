8876ca22abdaa86162bf8fa3811b0a8f8055a09f
dsbowen <dsbowen@wharton.upenn.edu>
2019-09-15 07:10:26 -0600

    cleaned shell and app files

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 """Create experiment as a web application"""
 
-from survey import Start
+from survey import *
 
 from hemlock import create_app
+from hemlock.app import db
 
 settings = {
     'start': Start
@@ -13,4 +14,7 @@ app = create_app(settings)
 if __name__ == '__main__':
     app.run()
     
-import hemlock_shell
+@app.shell_context_processor
+def make_shell_context():
+    db.create_all()
+    return globals()
