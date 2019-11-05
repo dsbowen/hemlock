231d1a51429c854d0a198455f2da5a23d7edf807
dsbowen <dsbowen@wharton.upenn.edu>
2019-11-05 14:24:06 -0500

    update

--- hemlock/app/routes/researcher_routes.py
+++ hemlock/app/routes/researcher_routes.py
@@ -179,7 +179,7 @@ def create_data(btn):
 @bp.route('/logout')
 @researcher_login_required
 def logout():
-    session.remove(password_key(), None)
+    session.pop(password_key(), None)
     return redirect(url_for('hemlock.login'))
 
 """
