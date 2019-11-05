87cc206420ecfad724a6b9c76726d01c9b9eb90d
dsbowen <dsbowen@wharton.upenn.edu>
2019-11-05 12:57:09 -0500

    update

--- hemlock/question_polymorphs/free.py
+++ hemlock/question_polymorphs/free.py
@@ -18,7 +18,7 @@ class Free(Question):
         return super()._render(content=INPUT.format(q=self))
     
     def _record_response(self, response):
-        print(response)
+        print('response is', response)
         self.response = None if response == [''] else response[0]
 
 INPUT = """
