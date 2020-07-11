845e04775508dde10fd54c053bf151ef4c5051d5
dsbowen <dsbowen@wharton.upenn.edu>
2020-07-11 11:21:52 -0400

    updated socket url https request in prod

--- hemlock/app/static/js/default.js
+++ hemlock/app/static/js/default.js
@@ -25,4 +25,13 @@ $("form").submit( function(e) {
 
 // Get socket.io url
 // $SOCKET_URL = location.protocol+"//"+document.domain+':'+location.port
-$SOCKET_URL = window.location.origin // TEST THIS
+// $SOCKET_URL = window.location.origin // TEST THIS
+console.log('location protocol is', location.protocol)
+console.log('documetnion domain is', document.domain)
+console.log('port is', location.port)
+if (document.domain == 'localhost') {
+    $SOCKET_URL = window.location.origin
+}
+else {
+    $SOCKET_URL = "https://"+document.domain
+}
