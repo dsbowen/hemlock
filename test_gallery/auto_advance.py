5f7567b03f4e1bd8c49fb7325137ef2a68bdfdae
dsbowen <dsbowen@wharton.upenn.edu>
2021-12-15 13:40:34 -0500

    Added js to prevent multiple form submissions.

--- test_gallery/auto_advance.py
+++ test_gallery/auto_advance.py
@@ -5,14 +5,14 @@ from hemlock.questions import Label
 @User.route("/survey")
 def seed():
     return [
-        zeroeth_page:=Page(
+        zeroeth_page := Page(
             Label("Zeroeth page.")
         ),
         Page(
             Label(
                 "Remaining time: <span id='clock-id'></span>",
-                compile=compile.auto_advance(10000, clock_id="clock-id")
-            ),
+                compile=compile.auto_advance(5000, clock_id="clock-id")
+            )
         ),
         Page(
             Label("The End."),
